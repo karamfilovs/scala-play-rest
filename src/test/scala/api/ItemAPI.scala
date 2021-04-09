d34c7a810e85a7d45378e6727c96178fa8e72ca1
Aleksandar Karamfilov <aleksandar.karamfilov.extern@joyn.de>
2021-04-09 16:35:39 +0300

    Added test for Items

--- src/test/scala/api/ItemAPI.scala
+++ src/test/scala/api/ItemAPI.scala
@@ -3,11 +3,23 @@ package api
 import play.api.libs.ws.WSResponse
 
 class ItemAPI extends HTTPClient {
-  private val ItemsUrl = "/clients"
-  private val ItemUrl = "/client"
+  private val ItemsUrl = "/items"
+  private val ItemUrl = "/item"
 
   def getAllItems: WSResponse = get(ItemsUrl)
 
   def getItem(id: Int): WSResponse = get(s"$ItemUrl/$id")
 
+  def createItem(name: String, price: Int, unit: String): WSResponse = {
+    val body =
+      s"""{
+         |    "name": "$name",
+         |    "price_for_quantity": "$price",
+         |    "quantity_unit": "$unit"
+         |}""".stripMargin
+
+    post(ItemUrl, body)
+  }
+
+  def deleteItem(id: Int): WSResponse = delete(s"$ItemUrl/$id")
 }
--- src/test/scala/specs/ItemSpec.scala
+++ src/test/scala/specs/ItemSpec.scala
@@ -2,16 +2,30 @@ package specs
 
 import api.API
 import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
+
+import java.time.LocalDateTime
 
 class ItemSpec extends PlaySpec {
   val api = new API
 
   "Items" should {
-    
+
     "be returned for user with valid credentials" in {
       val response = api.itemAPI.getAllItems
       response.status mustBe 200
       response.body must not be empty
+    }
+  }
+
+  "Item" should {
+    "be created with all mandatory fields" in {
+      val response = api.itemAPI.createItem("My Item" + LocalDateTime.now, 10, "кг.")
+      response.status mustBe 200
+      response.body must include ("Артикула е създаден успешно!")
+      val json = Json.parse(response.body)
+      val id = (json \ "success" \ "id").as[Int]
+      api.itemAPI.deleteItem(id)
     }
   }
 }
